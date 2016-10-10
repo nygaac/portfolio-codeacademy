f83a4c7a0566ceeaff53f6bd6a0a1e5929c6da9f
codecademy <ccuser@codecademy.com>
2016-10-10 05:11:21 -0400

    about page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,12 @@
 class PagesController < ApplicationController
   
   def welcome
-    
   end
   
   def portfolio
-    
+  end
+  
+  def about
   end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,10 @@ Rails.application.routes.draw do
   get '/portfolio' => 'pages#portfolio', as: :portfolio
   get '/' => 'pages#welcome', as: :home
   get "/blog" => redirect("http://www.christoffer-n.com"), :as => :blog
+  get '/about' => 'pages#about', as: :about
   resources :portfolio
   resources :welcome
+  resources :about
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
